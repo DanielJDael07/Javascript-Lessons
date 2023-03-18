4470ac01521e829302c8f7c3e596f9a97602636d
Daniel Dael <danieldael7777@gmail.com>
2023-03-19 06:50:40 +0800

    First commit

---
+++ JSexercise1.js
@@ -0,0 +1,24 @@
+let fullName = "Daniel Dael";
+
+fullname = "Geliezer Miranda";
+
+let numberVariable = 18.6;
+// Number Variable
+let booleanVariable = false;
+// Boolean Variable
+let arrayVariable = ["Honda", "Toyota", "Geely"];
+// Array Variable
+alert(arrayVariable[2]);
+// Alert Javascript Output
+document.write(fullname);
+// Document Write Output
+document.getElementById("headingSample").innerHTML = fullName;
+// Inner HTML Output
+console.log(numberVariable);
+// Console Log Output
+document.getElementsByClassName("sampleClass")[0].innerHTML = "GOTCHA";
+// Selecting Element using Class Name
+let classValuesGetter =
+  document.getElementsByClassName("sampleClass")[0].innerHTML;
+// Assigning an acquired element in a variable
+alert(classValuesGetter);
